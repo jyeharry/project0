ceab0361ae653e3a3e32ca9b79065a8e8224db6c
Jye Harry <jyeharry21@hotmail.com>
2021-01-05 11:39:00 +1100

    finally resolved the checkResult function

--- js/main.js
+++ js/main.js
@@ -11,15 +11,13 @@ $(document).ready(function() {
 let clickCounter = 0;
 const symbols = ['X', 'O'];
 
-
 const takeTurn = function() {
   if ($(this).html() === '') {
     $(this).html(symbols[clickCounter % 2]);
-    if (clickCounter >= 4 && checkResult()) {
-      updateResult();
-      // $($squares).each(function() {
-      //   $(this).off('click');
-      // });
+    if (checkResult()) {
+      endGame(true);
+    } else if (clickCounter === 8) {
+      endGame(false);
     }
     clickCounter++;
   }
@@ -27,29 +25,43 @@ const takeTurn = function() {
 
 const checkResult = function() {
   for (let i = 0; i < 3; i++) {
-    if ($($squares[i * 4]).text().length > 0) {
-      return checkRow(i * 3) || checkColumn(i) || checkDiagonal(i);
+    if (checkRow(i * 3) || checkColumn(i) || checkDiagonal(i)) {
+      return true;
     }
   }
+  return false;
 }
 
-const updateResult = function() {
-  $('#result').text(symbols[clickCounter % 2] + ' wins!');
+const endGame = function(win) {
+  const result = win ? symbols[clickCounter % 2] + ' wins!' : 'It\'s a draw!';
+  $('#result').text(result);
+  $($squares).each(function() {
+    $(this).off('click');
+  });
 }
 
 const checkRow = function(i) {
-  return $($squares[i]).text() === $($squares[i + 1]).text() && $($squares[i]).text() === $($squares[i + 2]).text();
+  if ($($squares[i]).text() !== '') {
+    return $($squares[i]).text() === $($squares[i + 1]).text() && $($squares[i]).text() === $($squares[i + 2]).text();
+  }
+  return false;
 }
 
 const checkColumn = function(i) {
-  return $($squares[i]).text() === $($squares[i + 3]).text() && $($squares[i]).text() === $($squares[i + 6]).text();
+  if ($($squares[i]).text() !== '') {
+    return $($squares[i]).text() === $($squares[i + 3]).text() && $($squares[i]).text() === $($squares[i + 6]).text();
+  }
+  return false;
 }
 
 const checkDiagonal = function(i) {
-  // this function only needs to get ran twice as there are only two diagonals, unlike checkRow and checkColumn, so we will skip this function by returning false when i is equal to 1
-  if (i === 1) {
+  // this function only needs to get ran twice as there are only two diagonals, unlike checkRow and checkColumn, so we will skip this function when i is equal to 1
+  if (i !== 1) {
+    // the indexes for the diagonals are 0, 4, 8 and 2, 4, 6, so we have to change the increment according to the diagonal we are checking
+    let increment = i === 0 ? 4 : 2;
+    if ($($squares[i]).text() !== '') {
+      return $($squares[i]).text() === $($squares[i + increment]).text() && $($squares[i]).text() === $($squares[i + 2 * increment]).text();
+    }
     return false;
   }
-  let increment = i % 4 === 0 ? 4 : 2;
-  return $($squares[i]).text() === $($squares[i + increment]).text() && $($squares[i]).text() === $($squares[i + 2 * increment]).text();
 }
