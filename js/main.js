c16288d238f1db8b6c0fb0e44ee06910437a09a6
Jye Harry <jyeharry21@hotmail.com>
2021-01-05 21:51:43 +1100

    finished project. just need to make it shinier

--- js/main.js
+++ js/main.js
@@ -1,33 +1,31 @@
 let $squares;
-const squares = [];
+let clickCounter = 0;
+const symbols = ['X', 'O'];
+let currentPlayer;
+let gameOver = false;
 
 $(document).ready(function() {
   $squares = $('td');
 
-  $('td').each(function(i) {
-    squares[i] = $(this);
-    squares[i].on('click', takeTurn);
-  });
+  $($squares).on('click', takeTurn);
 
   $('#new-game').on('click', newGame);
 });
 
-let clickCounter = 0;
-const symbols = ['X', 'O'];
-let gameOver = false;
-
+// resets the game board and variables
 const newGame = function() {
-  $($squares).each(function() {
-    $(this).html('');
-  });
+  $($squares).html('');
   clickCounter = 0;
   gameOver = false;
-  $('#result').text('');
+  $('#result').addClass('opacity');
 }
 
 const takeTurn = function() {
   if ($(this).text() === '' && !gameOver) {
-    $(this).text(symbols[clickCounter % 2]);
+    // whenever clickCounter is even, currentPlayer will be X and whenever clickCounter is odd currentPlayer will be O
+    currentPlayer = symbols[clickCounter % 2]
+    $(this).text(currentPlayer);
+    // check if someone has won, otherwise check if all the squares have been filled
     if (checkResult()) {
       endGame(true);
     } else if (clickCounter === 8) {
@@ -37,6 +35,13 @@ const takeTurn = function() {
   }
 }
 
+// ends the game, setting the result text accordingly
+const endGame = function(win) {
+  const result = win ? currentPlayer + ' wins!' : 'It\'s a draw!';
+  $('#result').text(result).removeClass('opacity');
+  gameOver = true;
+}
+
 const checkResult = function() {
   for (let i = 0; i < 3; i++) {
     if (checkRow(i * 3) || checkColumn(i) || checkDiagonal(i)) {
@@ -44,16 +49,6 @@ const checkResult = function() {
     }
   }
   return false;
-}
-
-const endGame = function(win) {
-  const result = win ? symbols[clickCounter % 2] + ' wins!' : 'It\'s a draw!';
-  $('#result').text(result);
-  gameOver = true;
-}
-
-const compareSquares = function(i, increment) {
-  return $($squares[i]).text() === $($squares[i + increment]).text() && $($squares[i]).text() === $($squares[i + 2 * increment]).text();
 }
 
 const checkRow = function(i) {
@@ -69,10 +64,15 @@ const checkColumn = function(i) {
 }
 
 const checkDiagonal = function(i) {
-  // this function only needs to get ran twice as there are only two diagonals, unlike checkRow and checkColumn, so we will skip this function when i is equal to 1.
   // the indexes for the diagonals are 0, 4, 8 and 2, 4, 6, so we have to change the increment according to the first index of the diagonal we are checking
   const increment = i === 0 ? 4 : 2;
+  // this function only needs to get ran twice as there are only two diagonals, unlike checkRow and checkColumn, so we will skip this function when i is equal to 1.
   if (i !== 1) {
     return $($squares[i]).text() !== '' ? compareSquares(i, increment) : false;
   }
+}
+
+// returns true if it finds three squares in a line with the same symbol and false otherwise
+const compareSquares = function(i, increment) {
+  return $($squares[i]).text() === $($squares[i + increment]).text() && $($squares[i]).text() === $($squares[i + 2 * increment]).text();
 }
