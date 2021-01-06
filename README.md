3625238fc1a3f29e8199fda6e277da05eadfd2a8
Jye Harry <jyeharry21@hotmail.com>
2021-01-06 17:43:22 +1100

    added select menus with functionality to hide selected options from other select menu

---
+++ README.md
@@ -0,0 +1,7 @@
+# General Assembly Project 0
+## TicTacToe
+See a demo of my TicTacToe game [here]().
+
+This was an enjoyable little project. Despite how simple TicTacToe is, this game took some serious head scratching to fully implement.
+
+I'm very certain that I'll come back to this project in the future and scoff at how poorly I approached it, but looking at bad code and knowing how I can improve it is one of the things I love about coding in general.
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let $squares;
-let clickCounter = 0;
+let moves = 0;
 const symbols = ['X', 'O'];
 let currentPlayer;
 let gameOver = false;
@@ -10,35 +10,47 @@ $(document).ready(function() {
   $($squares).on('click', takeTurn);
 
   $('#new-game').on('click', newGame);
+
+  $('select').change(updateSelect);
 });
 
 // resets the game board and variables
 const newGame = function() {
   $($squares).html('');
-  clickCounter = 0;
+  moves = 0;
   gameOver = false;
   $('#result').addClass('opacity');
 }
 
+const updateSelect = function() {
+  const value = $(this).val();
+  const id = parseInt($(this).attr('id'));
+  const nextId = (id + 1) % 2;
+  const optionToHide = $(`select option[value=${value}]`);
+  $(`#${nextId} option`).show();
+  $(optionToHide[nextId]).hide();
+}
+
 const takeTurn = function() {
   if ($(this).text() === '' && !gameOver) {
-    // whenever clickCounter is even, currentPlayer will be X and whenever clickCounter is odd currentPlayer will be O
-    currentPlayer = symbols[clickCounter % 2]
+    // whenever moves is even, currentPlayer will be X and whenever moves is odd currentPlayer will be O
+    currentPlayer = symbols[moves % 2]
     $(this).text(currentPlayer);
-    // check if someone has won, otherwise check if all the squares have been filled
+    // check if someone has won, otherwise check if all the squares have been filled then end the game if either condition is met
     if (checkResult()) {
       endGame(true);
-    } else if (clickCounter === 8) {
+    } else if (moves === 8) {
       endGame(false);
     }
-    clickCounter++;
+    moves++;
   }
 }
 
 // ends the game, setting the result text accordingly
-const endGame = function(win) {
-  const result = win ? currentPlayer + ' wins!' : 'It\'s a draw!';
-  $('#result').text(result).removeClass('opacity');
+const endGame = function(gameWon) {
+  const result = gameWon ? currentPlayer + ' wins!' : 'It\'s a draw!';
+  $('#result').text(result)
+              .removeClass('opacity');
   gameOver = true;
 }
 
